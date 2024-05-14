9ad746a58602c0dbbb5eebb1d78e668049339cd7
Alex Tu <alextutruong@gmail.com>
2024-05-14 01:39:51 -0400

    Fixed team finding

--- src/main.py
+++ src/main.py
@@ -1,14 +1,17 @@
-from utils import scraper
+from utils import scraper, SMS
 
-TEAM = "Virtus.pro"
+# TEAM = "Virtus.pro"
+TEAM = "ODDIK"
 
 
 matches = scraper.matches_today()
 
 matches_with_team = []
 for match in matches:
-    if match["team1"] == TEAM or match["team2"] == TEAM:
+    teams = [match["Team 1"], match["Team 2"]]
+    if TEAM in teams:
         matches_with_team.append(match)
 
 for game in matches_with_team:
-    print(game)
+    message = f"{game['Series']}, {game['Team 1']} vs {game['Team 2']} @ {game['Time']}"
+    print(message)
