24515e01b045e5a058c4da55077e79ff986d887e
Alex Tu <alextutruong@gmail.com>
2024-05-14 00:11:08 -0400

    Fixed temp time workaround

--- src/utils/scraper.py
+++ src/utils/scraper.py
@@ -3,39 +3,47 @@ from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 
 
-URL = 'https://www.hltv.org/'
+URL = "https://www.hltv.org/"
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
 }
 
 
 def matches_today():
-    response = requests.get(URL, headers=headers,  timeout=30)
+    response = requests.get(URL, headers=headers, timeout=30)
 
     if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
+        soup = BeautifulSoup(response.text, "html.parser")
 
-        matches = soup.find('h1', class_='clearfix todaysMatches').find_parent('aside').find_all('a')[1:]
+        matches = (
+            soup.find("h1", class_="clearfix todaysMatches")
+            .find_parent("aside")
+            .find_all("a")[1:]
+        )
 
         match_dicts = []
         for match in matches:
 
-            teams = match.find_all('div', class_='teamrow')
-            date = int(match.find('div', class_='middleExtra').get('data-unix') if match.find('div', class_='middleExtra') else '0')
+            teams = match.find_all("div", class_="teamrow")
+            date = int(
+                match.find("div", class_="middleExtra").get("data-unix")
+                if match.find("div", class_="middleExtra")
+                else "0"
+            )
 
             if date == 0 or within_day(date):
                 if date == 0:
-                    time = 'Live'
+                    time = "Live"
                 else:
-                    date_time = datetime.fromtimestamp(date/1000)
-                    time = date_time.strftime('%H:%M')
+                    date_time = datetime.fromtimestamp(date / 1000)
+                    time = date_time.strftime("%H:%M")
 
                 dict = {
-                    'Series': match.get('title'),
-                    'Team 1': teams[0].text,
-                    'Team 2': teams[1].text,
-                    'Date/Time': time
+                    "Series": match.get("title"),
+                    "Team 1": teams[0].text,
+                    "Team 2": teams[1].text,
+                    "Date/Time": time,
                 }
                 print(dict)
 
@@ -45,9 +53,8 @@ def matches_today():
 
 
 def within_day(unix_timestamp):
-    date_time = datetime.fromtimestamp(unix_timestamp/1000)
+    date_time = datetime.fromtimestamp(unix_timestamp / 1000)
     now = datetime.now().date()
-    now = now + timedelta(days=1)
     day_start = datetime.combine(now, datetime.min.time())
     day_end = datetime.combine(now, datetime.max.time())
 
