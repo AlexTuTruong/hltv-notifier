36113dbd9de141f6bdf5e2b34797dbcb02149fc9
Alex Tu <alextutruong@gmail.com>
2024-05-14 00:12:10 -0400

    Updated main.py to find team matches

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
 from utils import scraper
 
+TEAM = "Virtus.pro"
+
+
 matches = scraper.matches_today()
 
+matches_with_team = []
 for match in matches:
-    print(match)
+    if match["team1"] == TEAM or match["team2"] == TEAM:
+        matches_with_team.append(match)
+
+for game in matches_with_team:
+    print(game)
